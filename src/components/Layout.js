f18de111111cf012598f853602914c9b103cf27f
shooshte <miha.sustersic.work@gmail.com>
2018-10-20 11:22:51 +0200

    switched back to sass from react-emotion

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,27 +1,16 @@
 import React from "react";
-import styled from "react-emotion";
 
 import Helmet from "react-helmet";
 import Navbar from "../components/Navbar";
 
-import { COLORS, SIZES } from "../styles/constants";
-
-const Container = styled("div")`
-  background-color: ${COLORS.light};
-  color: ${COLORS.dark};
-  max-width: ${SIZES.screenSize};
-  margin-left: calc((100% - ${SIZES.screenSize}) * 0.6);
-  margin-top: 58px;
-  padding: 1rem;
-  overflow: auto;
-`;
+import "../styles/layout.sass";
 
 const TemplateWrapper = ({ children }) => (
-  <Container>
+  <div className="layout-container">
     <Helmet title="topdeckAndWreck" />
     <Navbar />
     <div>{children}</div>
-  </Container>
+  </div>
 );
 
 export default TemplateWrapper;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,37 +1,10 @@
 import React from "react";
 import { Link } from "gatsby";
-import styled from "react-emotion";
 
-import { COLORS, SIZES } from "../styles/constants";
-
-const Container = styled("nav")`
-  position: fixed;
-  padding: 1rem;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  background-color: ${COLORS.main};
-  .links-container {
-    max-width: ${SIZES.screenSize};
-    width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: flex-end;
-  }
-  .nav-link {
-    color: ${COLORS.light};
-    padding: 0 1rem;
-    text-decoration: none;
-  }
-  @media only screen and (min-width: 60ch) {
-    .links-container {
-      margin-left: calc((100% - ${SIZES.screenSize}) * 0.6);
-    }
-  }
-`;
+import "../styles/navbar.sass";
 
 const Navbar = () => (
-  <Container>
+  <nav className="navbar-container">
     <div className="links-container">
       <Link to="/" className="nav-link">
         Blog
@@ -40,7 +13,7 @@ const Navbar = () => (
         About
       </Link>
     </div>
-  </Container>
+  </nav>
 );
 
 export default Navbar;
